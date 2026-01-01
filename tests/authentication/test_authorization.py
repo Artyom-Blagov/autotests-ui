7ada9d3253f8a168d87b5761b665a361c5f9dad5
Artyom-Blagov <gameysmile94@gmail.com>
2026-01-01 22:10:12 +0200

    8.2 Интеграция Allure в автотесты. Практическая работа с Allure аннотациями

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -21,6 +21,9 @@ from tools.allure.stories import AllureStory
 @allure.epic(AllureEpic.LMS)
 @allure.feature(AllureFeature.AUTHENTICATION)
 @allure.story(AllureStory.AUTHORIZATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthorization:
     @allure.tag(AllureTag.USER_LOGIN)
     @allure.title('Успешная авторизация с корректной почтой и паролем')
