49793b29aa35951605d6942f51f5758e7437ef5c
Artyom-Blagov <gameysmile94@gmail.com>
2026-01-07 15:23:00 +0200

    9.5 Логирование взаимодействий со страницей. Практика работы с логированием

--- elements/file_input.py
+++ elements/file_input.py
@@ -1,5 +1,8 @@
-from elements.base_element import BaseElement
 import allure
+from elements.base_element import BaseElement
+from tools.logger import get_logger
+
+logger = get_logger("FILE_INPUT_ELEMENT")
 
 class FileInput(BaseElement):
     @property
@@ -7,6 +10,9 @@ class FileInput(BaseElement):
         return 'file input'
 
     def set_input_files(self, file: str,nth: int = 0, **kwargs):
-        with allure.step(f'Ste file {file} to the {self.type_of} "self.name"'):
+        step = f'Ste file {file} to the {self.type_of} "self.name"'
+
+        with allure.step(step):
             locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
             locator.set_input_files(file)
